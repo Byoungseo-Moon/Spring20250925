e85023dc266ef082a1b7b28b7cc4ecc77b6e4ab9
Byoungseo-Moon <bsmoonbs@gmail.com>
2025-09-26 02:14:11 +0900

    Update BoardController.java
    불필요 요소 삭제

--- test1/src/main/java/com/example/test1/controller/BoardController.java
+++ test1/src/main/java/com/example/test1/controller/BoardController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.test1.dao.BoardService;
-import com.example.test1.dao.StudentService;
-import com.example.test1.dao.UserService;
 import com.google.gson.Gson;
 
 @Controller
@@ -23,20 +21,10 @@ public class BoardController {
 	
 	@RequestMapping("/board-list.do") 
     public String list(Model model) throws Exception{
-
+		
         return "/board-list";
-    }
-	
-//	@RequestMapping(value = "/stu-info.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-//	@ResponseBody
-//	public String studentinfo(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
-//		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-//		
-//		resultMap = studentService.studentInfo(map);		
-//		
-//		return new Gson().toJson(resultMap);
-//	}
-//	
+    }	
+
 	@RequestMapping(value = "/board-list.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String boardlist(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
