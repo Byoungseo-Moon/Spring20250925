0fcae79a02cbbec5c9fcf7d1c3304d68f806f3a8
Byoungseo-Moon <bsmoonbs@gmail.com>
2025-09-26 02:10:24 +0900

    Update BoardService.java
    불필요 요소 삭제

--- test1/src/main/java/com/example/test1/dao/BoardService.java
+++ test1/src/main/java/com/example/test1/dao/BoardService.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.test1.controller.StuController;
+
 import com.example.test1.mapper.BoardMapper;
-import com.example.test1.mapper.StudentMapper;
-import com.example.test1.mapper.UserMapper;
 import com.example.test1.model.Board;
-import com.example.test1.model.Student;
-import com.example.test1.model.User;
 
 @Service
 public class BoardService {
@@ -25,7 +21,7 @@ public class BoardService {
 				
 			List<Board> board = boardMapper.boardList(map);
 						
-		resultMap.put("input", board);	
+		resultMap.put("list", board);	
 		resultMap.put("result", "success");
 			
 		return resultMap;
